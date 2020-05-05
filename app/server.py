abe659dfff59b5fb976f646c9c477c39b76a6f68
aarora91 <ashimaarora@Ashimas-MBP-2.fios-router.home>
2020-05-04 23:11:03 -0400

    Updated to my model

--- app/server.py
+++ app/server.py
@@ -7,9 +7,9 @@ from io import BytesIO
 
 from fastai.vision import *
 
-model_file_url = 'https://www.dropbox.com/s/y4kl2gv1akv7y4i/stage-2.pth?raw=1'
-model_file_name = 'model'
-classes = ['black', 'grizzly', 'teddys']
+model_file_url = 'https://drive.google.com/uc?export=download&id=1lgE39h5zMXQCJ-zU7AfuAsRFAfxMLIwI'
+model_file_name = 'stage-1'
+classes = ['masked', 'unmasked', 'invalid']
 path = Path(__file__).parent
 
 app = Starlette()
